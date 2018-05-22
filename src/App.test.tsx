595b01fdfce8d900efb8ade76392f9abbbd7bc5b
alexander.svendsen <alexandersvendsen1990@gmail.com>
2018-05-22 16:02:27 +0200

    Disable tslint for hacky config in tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-window['HTMLElement'].prototype.scrollIntoView = function() {};
+/* tslint:disable */
+window['HTMLElement'].prototype.scrollIntoView = () => {};
+/* tslint:enable */
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
