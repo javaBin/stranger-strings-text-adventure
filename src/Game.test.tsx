22ca670136a9403219a5ff2dde8dd45c071c4ccb
alexander-svendsen <alexandersvendsen1990@gmail.com>
2018-08-09 21:02:52 +0200

    Fix render test
    Need to mock gtag before importing game

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,8 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+(global as any).gtag = () => undefined;
+
+import Game from './Game';
 
 (window as any).HTMLElement.prototype.scrollIntoView = () => undefined;
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<Game />, div); //todo fix me
-  // ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  ReactDOM.render(<Game />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
