b6d2e28f721619eee29000245ddad853d9b7418c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-09 11:04:10 +0000

    Deploy javaBin/stranger-strings-text-adventure to github.com/javaBin/stranger-strings-text-adventure.git:gh-pages

---
+++ precache-manifest.5d15a16eb6ee7883fadc2361c1762111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "234139ebd4e9f7273d51",
+    "url": "/static/css/main.37610f36.chunk.css"
+  },
+  {
+    "revision": "234139ebd4e9f7273d51",
+    "url": "/static/js/main.234139eb.chunk.js"
+  },
+  {
+    "revision": "cd3ef30756fc8026efa5",
+    "url": "/static/js/1.cd3ef307.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac274592db49163484a528d132f03008",
+    "url": "/index.html"
+  }
+];
